6d1006fd6567a920ad359b26fa0cd8b1f05204f5
Alewep <alex.superun@gmail.com>
2021-02-02 10:37:34 +0100

    refonte

--- arbre/main.cpp
+++ arbre/main.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
-#include "arbre.h"
+
 using namespace std;
 
 int main()
 {
-    arraytree a = {{false,1},{true,0},{false,2},{true,0},{true,0},{false,3},{false,0},};
-    //affichertableau(a);
-    binarytree b = to_binarytree(a);
-    parcourprefixe(b);
-    a.clear();
-    std::cout<<std::endl;
-    to_arraytree(b,a);
-    narytree n = to_narytree(b);
-    parcourprefixe(n);
-    //affichertableau(a);
-
+    cout << "Hello World!" << endl;
+    return 0;
 }
